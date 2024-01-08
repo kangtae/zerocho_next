23e340a1733b743b5b09388a0d87cf79e216a072
kangtaewan <bras1717@naver.com>
2024-01-08 18:56:30 +0900

    세션 2작업 커밋 완료

--- src/app/(afterLogin)/_component/ActionButtons.tsx
+++ src/app/(afterLogin)/_component/ActionButtons.tsx
@@ -2,18 +2,25 @@
 import style from './post.module.css';
 import cx from 'classnames';
 
-export default function ActionButtons() {
+type Props = {
+	white?: boolean
+}
+
+export default function ActionButtons({white}: Props) {
 	const commented = true;
 	const reposted = true;
 	const liked = false;
 
-	const onClickComment = () => {}
-	const onClickRepost = () => {}
-	const onClickHeart = () => {}
+	const onClickComment = () => {
+	}
+	const onClickRepost = () => {
+	}
+	const onClickHeart = () => {
+	}
 
 	return (
 		<div className={style.actionButtons}>
-			<div className={cx(style.commentButton, { [style.commented]: commented })}>
+			<div className={cx(style.commentButton, {[style.commented]: commented}, white && style.white)}>
 				<button onClick={onClickComment}>
 					<svg width={24} viewBox="0 0 24 24" aria-hidden="true">
 						<g>
@@ -24,7 +31,7 @@ export default function ActionButtons() {
 				</button>
 				<div className={style.count}>{1 || ''}</div>
 			</div>
-			<div className={cx(style.repostButton, reposted && style.reposted)}>
+			<div className={cx(style.repostButton, reposted && style.reposted, white && style.white)}>
 				<button onClick={onClickRepost}>
 					<svg width={24} viewBox="0 0 24 24" aria-hidden="true">
 						<g>
@@ -35,7 +42,7 @@ export default function ActionButtons() {
 				</button>
 				<div className={style.count}>{1 || ''}</div>
 			</div>
-			<div className={cx([style.heartButton, liked && style.liked])}>
+			<div className={cx([style.heartButton, liked && style.liked, white && style.white])}>
 				<button onClick={onClickHeart}>
 					<svg width={24} viewBox="0 0 24 24" aria-hidden="true">
 						<g>
