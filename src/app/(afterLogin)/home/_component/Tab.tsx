8194632c195f2834ed042adda987a0ecff348a26
kangtaewan <bras1717@naver.com>
2024-01-08 19:18:08 +0900

    세션 3 시작전 제로초 리베이스

--- src/app/(afterLogin)/home/_component/Tab.tsx
+++ src/app/(afterLogin)/home/_component/Tab.tsx
@@ -3,28 +3,28 @@ import style from './tab.module.css';
 import {useState} from "react";
 
 export default function Tab() {
-	const [tab, setTab] = useState('rec');
+  const [tab, setTab] = useState('rec');
 
-	const onClickRec = () => {
-		setTab('rec');
-	}
-	const onClickFol = () => {
-		setTab('fol');
-	}
+  const onClickRec = () => {
+    setTab('rec');
+  }
+  const onClickFol = () => {
+    setTab('fol');
+  }
 
-	return (
-		<div className={style.homeFixed}>
-			<div className={style.homeText}>홈</div>
-			<div className={style.homeTab}>
-				<div onClick={onClickRec}>
-					추천
-					<div className={style.tabIndicator} hidden={tab === 'fol'}></div>
-				</div>
-				<div onClick={onClickFol}>
-					팔로우 중
-					<div className={style.tabIndicator} hidden={tab === 'rec'}></div>
-				</div>
-			</div>
-		</div>
-	)
+  return (
+    <div className={style.homeFixed}>
+      <div className={style.homeText}>홈</div>
+      <div className={style.homeTab}>
+        <div onClick={onClickRec}>
+          추천
+          <div className={style.tabIndicator} hidden={tab === 'fol'}></div>
+        </div>
+        <div onClick={onClickFol}>
+          팔로우 중
+          <div className={style.tabIndicator} hidden={tab === 'rec'}></div>
+        </div>
+      </div>
+    </div>
+  )
 }
